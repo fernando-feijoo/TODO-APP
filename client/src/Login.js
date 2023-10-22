682c88b03d490126c0b1a6bc072e0d726ee65e30
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-10-22 15:01:04 -0500

    React Proyect Created

---
+++ client/src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <div>
+            <form>
+                <div>
+                    <laber htmlFor="email">Email</laber>
+                    <input type='email' placeholder='Enter Email'></input>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
