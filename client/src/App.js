dfe9b6cbd714223a9f0d06af24fb6523bfeebd69
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-10-22 19:49:48 -0500

    Insert ok more or less.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,71 @@
 import "./App.css";
 import { useState } from "react";
+import Axios from "axios";
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
-  const [status, setStatus] = useState("");
+  const [status, setStatus] = useState(false); // Inicializado como false
 
-  const showTitle = () => {
-    alert(status)
+  const add = () => {
+    const completedValue = status === "on"; // Convertir "on" a true y cualquier otro valor a false
+    Axios.post("http://localhost:4000/todos", {
+      title: title,
+      description: description,
+      category: category,
+      completed: completedValue,
+    }).then(() => {
+      alert("New register added. ", completedValue);
+
+      // Restablecer los valores de los campos del formulario
+      setTitle("");
+      setDescription("");
+      setCategory("");
+      setStatus(false); // Opcionalmente, puedes reiniciar el estado del checkbox a "false"
+    });
   };
 
   return (
     <div className="App">
       <div className="datos">
         <label>
-          Title: <input 
-          onChange={(event)=>{
-            setTitle(event.target.value);
-          }}
-          type="text" />
+          Title:{" "}
+          <input
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+            type="text"
+          />
         </label>
         <label>
-          Description: <input 
-          onChange={(event)=>{
-            setDescription(event.target.value);
-          }}
-          type="text" />
+          Description:{" "}
+          <input
+            onChange={(event) => {
+              setDescription(event.target.value);
+            }}
+            type="text"
+          />
         </label>
         <label>
-          Category: <input 
-          onChange={(event)=>{
-            setCategory(event.target.value);
-          }}
-          type="text" />
+          Category:{" "}
+          <input
+            onChange={(event) => {
+              setCategory(event.target.value);
+            }}
+            type="text"
+          />
         </label>
         <label>
-          Completed/Incomplete: <input 
-          onChange={(event)=>{
-            setStatus(event.target.value);
-          }}
-          type="checkbox" />
+          Completed/Incomplete:{" "}
+          <input
+            onChange={(event) => {
+              setStatus(event.target.value);
+            }}
+            type="checkbox"
+          />
         </label>
-        <button onClick={showTitle}>Save</button>
+        <button onClick={add}>Save</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -2,18 +2,24 @@ import express from 'express';
 import { config } from 'dotenv';
 import pg from 'pg';
 import router from './router.js';
+import cors from 'cors';
 
 config();
 
 const app = express();
+
+app.use(cors());
+
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.SSL_ENABLED === 'true' ? true : false
 });
 
+app.use(express.json());
 
 app.use('/', router);
 app.use('/ping', router);
+app.use('/todos', router);
 
 app.listen(4000);
 console.log('Server on port ', 4000);
